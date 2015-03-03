27c6259fd0e9972d6d7ca2e32da2d51c1fdc714f
David Goldberg <DBGoldberg01@gmail.com>
2015-03-03 00:13:13 -0500

    abitlity to add new infos

--- from_meteor_book/client/controllers/modal.js
+++ from_meteor_book/client/controllers/modal.js
@@ -25,17 +25,19 @@ Template.modal.helpers({
 });
 Template.modal.events({
 	'dblclick #node-title': change_node_title,
-	'focusout #node-title-input': commit_node_title,
+	'blur #node-title-input': commit_node_title,
 	'keydown #node-title-input': function(e){
 		if (e.which == 13) commit_node_title();
 	},
 	'click .color_box': color_node,
 	'dblclick .panel-heading': change_info_title,
-	//'focusout .panel-title-input': commit_info_title,
-	// 'keydown .panel-title-input': function(e){
-	// 	if (e.which == 13) commit_info_title();
-	// },
-	// 'focusout .panel-body': commit_panel_body
+	'blur .info-title-input': commit_info_title,
+	'keydown .info-title-input': function(e){
+		if (e.which == 13) commit_info_title();
+	},
+	'click .panel-body': change_info_content,
+	'blur .info-data-input': commit_info_content,
+	'click #new_info': create_new_info
 });
 
 function change_node_title(){
@@ -44,7 +46,7 @@ function change_node_title(){
 }
 function commit_node_title(){
 	var new_title = $('#node-title-input').val();
-	if(new_title.length > 0){
+	if(new_title && new_title.length > 0){
 		Experinode.Nodes.change_title(this._id, new_title);
 	}
 	$("#node-title").removeClass("hidden");
@@ -58,6 +60,35 @@ function color_node(e) {
 	}
 }
 function change_info_title(){
-	$(".panel-title").addClass("hidden");
-	$('#info-title-input').removeClass("hidden");
+	$("#info-title-" + this._id).addClass("hidden");
+	$('#info-title-input-'+ this._id).removeClass("hidden");
+}
+function commit_info_title(){
+	var $input = $("#info-title-input-" + this._id);
+	var new_title = $input.val();
+	if(new_title && new_title.length > 0){
+		Experinode.Infos.change_title(this._id, new_title);
+	}
+	$("#info-title-" + this._id).removeClass("hidden");
+	$input.addClass("hidden");
+}
+function change_info_content(){
+	$("#info-data-" + this._id).addClass("hidden");
+	$('#info-data-input-'+ this._id).removeClass("hidden");
+}
+function commit_info_content(){
+	var $input = $("#info-data-input-" + this._id);
+	var new_data = $input.val();
+	if(new_data && new_data.length > 0){
+		Experinode.Infos.change_data(this._id, new_data);
+	}
+	$("#info-data-" + this._id).removeClass("hidden");
+	$input.addClass("hidden");
+}
+function create_new_info(){
+	Experinode.Infos.create(
+		"Double Click to change Title",
+		"Double Click to add some data",
+		this._id
+	);
 }
